a91774adc9e3a00d455fae8cd2e515e5f65aeb51
Eduardo Ramirez <edvramirezr@gmail.com>
2020-04-16 16:54:41 +0200

    Arreglo en el formato del archivo

--- game.py
+++ game.py
@@ -18,6 +18,18 @@ class Entity:
         self.mass = 0
         self.radius = 0
 
+    @staticmethod
+    def move(x, y, thrust):
+        return f'MOVE {x} {y} {thrust}'
+
+    @staticmethod
+    def throw(x, y, power):
+        return f'THROW {x} {y} {power}'
+
+    @staticmethod
+    def spell(entity_id, x, y, magic):
+        return f'WINGARDIUM {id} {x} {y} {magic}'
+
 
 class Wizard(Entity):
     def __init__(self, arr):
@@ -28,6 +40,14 @@ class Wizard(Entity):
         super().__init__(arr)
         self.mass = 1.0
         self.radius = 400
+
+    def defend(self, game):
+        """
+
+        :param Game game: variable with all information about the game state
+        :return: str: Action to be execute
+        """
+        return "MOVE 2000 3750 100"
 
 
 class Snaffle(Entity):
@@ -54,43 +74,61 @@ class Bludger(Entity):
 
 class Team:
     """Class to store information about a team"""
-    def __init__(self, arr):
+
+    def __init__(self, arr, goal):
         self.score = arr[0]
         self.magic = arr[1]
-        self.wizards = []       # type: list[Wizard]
+        self.wizards = []  # type: list[Wizard]
+        self.goal = goal  # type: Goal
 
 
 class Game:
     """Class to store the information for all entities from a game turn"""
+
     def __init__(self, player, enemy):
         """
 
         :param Team player:
         :param Team enemy:
         """
-        self.player = player    # type: Team
-        self.enemy = enemy      # type: Team
-        self.bludgers = []      # type: list[Bludger]
-        self.snaffles = []      # type: list[Snaffle]
+        self.player = player  # type: Team
+        self.enemy = enemy  # type: Team
+        self.bludgers = []  # type: list[Bludger]
+        self.snaffles = []  # type: list[Snaffle]
+
+
+class Goal:
+    def __init__(self, x, y):
+        self.x = x  # type: int
+        self.y = y  # type: int
+        self.top_pole = y - 2000  # type: int
+        self.bottom_pole = y + 2000  # type: int
+        self.pole_radius = 300  # type: int
 
 
 my_team_id = int(input())
+if my_team_id == 0:
+    player_goal = Goal(0, 3750)
+    enemy_goal = Goal(16000, 3750)
+else:
+    player_goal = Goal(16000, 3750)
+    enemy_goal = Goal(0, 3750)
 
 # game loop
 while True:
-    team1 = Team([int(i) for i in input().split()])
-    team2 = Team([int(i) for i in input().split()])
+    team1 = Team([int(i) for i in input().split()], player_goal)
+    team2 = Team([int(i) for i in input().split()], enemy_goal)
     entities = int(input())  # number of entities still in game
-    game = Game(team1, team2)
+    game_state = Game(team1, team2)
     for i in range(entities):
         entity = input().split()
         if entity[1] == 'WIZARD':
-            game.player.wizards.append(Wizard(entity))
+            game_state.player.wizards.append(Wizard(entity))
         elif entity[1] == 'BLUDGER':
-            game.bludgers.append(Bludger(entity))
+            game_state.bludgers.append(Bludger(entity))
         elif entity[1] == 'SNAFFLE':
-            game.snaffles.append(Snaffle(entity))
+            game_state.snaffles.append(Snaffle(entity))
         else:
-            game.enemy.wizards.append(Wizard(entity))
-    for wizard in game.player.wizards:
-        print("MOVE 8000 3750 100")
+            game_state.enemy.wizards.append(Wizard(entity))
+    print(game_state.player.wizards[0].defend(game_state))
+    print(game_state.player.wizards[1].defend(game_state))
